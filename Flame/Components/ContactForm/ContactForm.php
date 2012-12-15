d114ab81d04e4d79a694a342f4015eb1d588e02f
JSifalda <sifalda.jiri@gmail.com>
2012-12-15 18:21:37 +0100

    Add formSubmitted method

--- Flame/Components/ContactForm/ContactForm.php
+++ Flame/Components/ContactForm/ContactForm.php
@@ -13,13 +13,34 @@ namespace Flame\Components\ContactForm;
 class ContactForm extends \Flame\Application\UI\Form
 {
 
+	/**
+	 * @var ContactFormProcess $contactFormProcess
+	 */
+	private $contactFormProcess;
+
+	/**
+	 * @param ContactFormProcess $contactFormProcess
+	 */
+	public function injectContactFormProcess(ContactFormProcess $contactFormProcess)
+	{
+		$this->contactFormProcess = $contactFormProcess;
+	}
+
 	public function __construct()
 	{
 		parent::__construct();
 
 		$this->addExtension('addAntiSpam', '\Flame\Forms\Controls\AntiSpamControl');
-
 		$this->configure();
+		$this->onSuccess[] = $this->formSubmitted;
+	}
+
+	/**
+	 * @param ContactForm $form
+	 */
+	public function formSubmitted(ContactForm $form)
+	{
+		$this->contactFormProcess->send($form);
 	}
 
 	private function configure()
